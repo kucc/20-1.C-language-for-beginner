40a7a007b8af84d4ff5524efeaa5e114dd51b8d3
Jihyun <zzzmilky@naver.com>
2020-04-20 18:39:34 +0900

    10869

---
+++ Homework/강보미/10869.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int A;
+	int B;
+	
+	scanf("%d %d", &A, &B);
+	
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+
+}
