c6c8890801c3c5299706cdc065d82b9ad193699c
BbomiiK <kbmnl@naver.com>
2020-05-10 19:09:09 +0900

    Prob01.c

---
+++ Homework/강보미/homework01/Prob1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	printf("이름: %s\n", "강보미");
+	printf("성별: %c\n", "여");
+	printf("학번: %d\n", 2020100100);
+	printf("나이: %d살\n", 21);
+
+	return 0;
+}
